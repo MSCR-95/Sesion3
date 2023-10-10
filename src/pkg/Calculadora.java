501fdb9dfbb905e98edaa269dd6489a3e50de0b8
Maria S <Maria S@DESKTOP-QB1MIM4>
2023-10-10 16:27:44 +0200

    TDD paso 2 codigo minimo que pasa la prueba

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -2,4 +2,24 @@ package pkg;
 
 public class Calculadora {
 
+	public static Integer Suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	public static Integer Resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return -1;
+	}
+
+	public static Integer Multiplica(int i, int j) {
+		// TODO Auto-generated method stub
+		return 6;
+	}
+
+	public static Integer Divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
